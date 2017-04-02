f9ac91d771805e217c2b6e55d31ac8d900a51998
Florent Béjina <florent@bejina.fr>
2017-04-02 22:56:15 +0200

    minor bugfix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { default as defaultMedia } from './src/styled/media';
+export { default as media } from './src/styled/media';
 export { default as Container } from './src/Container';
 export { default as Row } from './src/Row';
 export { default as Col } from './src/Col';
