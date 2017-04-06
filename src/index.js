ed79fa7593709dfd94ae7002101dc3a4237e4fc2
Florent Béjina <florent@bejina.fr>
2017-04-06 12:33:51 +0200

    index bugfix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as media } from './src/styled/media';
-export { default as Container } from './src/Container';
-export { default as Row } from './src/Row';
-export { default as Col } from './src/Col';
+export { default as media } from './styled/media';
+export { default as Container } from './Container';
+export { default as Row } from './Row';
+export { default as Col } from './Col';
