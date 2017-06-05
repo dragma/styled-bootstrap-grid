c63be8551b5ad53475be2e638680b33001f3898c
Dragma78 <florent@bejina.fr>
2017-06-05 15:44:37 +0200

    add buildProps util

---
+++ src/utils/buildProps.js
@@ -0,0 +1,12 @@
+const (props, propsKeys = ['children']) => {
+  const otherProps =
+    Object
+      .keys(props)
+      .filter(key => propsKeys.indexOf(key) === -1)
+      .reduce((acc, key) => {
+          acc[key] = props[key];
+          return acc;
+        }, {}
+      );
+  return otherProps;
+}
